d1b858f897ca1875197004ed143cc8582aae87a9
unknown <jithinbp007@gmail.com>
2024-02-06 21:43:19 +0530

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,8 @@
+// import { headerLogo } from "../assets/images";
+// import { hamburger } from "../assets/icons";
+
+function Nav() {
+  return <header className="padding-x py-8 absolute z-10 w-full"></header>;
+}
+
+export default Nav;
